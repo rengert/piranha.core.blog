592067eef325debf1edbc834830bbbfd1db64893
Håkan Edling <hakan@tidyui.com>
2018-04-08 22:30:14 +0200

    Added solution. Downgraded Razor template to netcoreapp2 for now

--- src/razor/Startup.cs
+++ src/razor/Startup.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -29,17 +28,10 @@ namespace Blog
         // This method gets called by the runtime. Use this method to add services to the container.
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
             services.AddMvc(config => 
             {
                 config.ModelBinderProviders.Insert(0, new Piranha.Manager.Binders.AbstractModelBinderProvider());
-            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            });
             
             services.AddDbContext<Db>(options =>
                 options.UseSqlite("Filename=./piranha.db"));            
@@ -66,12 +58,7 @@ namespace Blog
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
-
+            
             // Initialize Piranha
             var api = services.GetService<IApi>();
             App.Init(api);
